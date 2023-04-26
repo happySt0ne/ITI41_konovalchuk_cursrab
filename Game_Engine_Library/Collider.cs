2b0422181e8f9a2af4e88bd31c2493aefae168cc
H1Kt0 <konovalchuk.denis@gmail.com>
2023-04-26 14:53:33 +0300

    Создан класс, позволяющий создавать collision box в форме прямоугольника для объектов, а также для проверки столкновения двух таких объектов.

--- Game_Engine_Library/Collider.cs
+++ Game_Engine_Library/Collider.cs
@@ -7,7 +7,32 @@ using System.Threading.Tasks;
 
 
 namespace Game_Engine_Library {
-    public static class Collision {
-        RectangleF asd; 
+    /// <summary>
+    /// Класс для проверки столкновения объектов.
+    /// </summary>
+    public class Collision {
+        private RectangleF _collisionBox;
+
+        /// <summary>
+        /// Инициализация прямоугольного collision box.
+        /// </summary>
+        /// <param name="x">Координата X верхнего левого угла прямоугольника.</param>
+        /// <param name="y">Координата Y верхнего левого угла прямоугольника.</param>
+        /// <param name="width">Ширина прямоугольника.</param>
+        /// <param name="height">Высота прямоугольника.</param>
+        public Collision(double x, double y, double width, double height) { 
+            _collisionBox = new RectangleF(Convert.ToSingle(x),
+                                           Convert.ToSingle(y),
+                                           Convert.ToSingle(width),
+                                           Convert.ToSingle(height));
+        }
+
+        /// <summary>
+        /// Проверка столкновения двух объектов.
+        /// </summary>
+        /// <param name="compareObject">Объект, с которым проверяется столкновение.</param>
+        /// <returns>true, если объекты столкнулись</returns>
+        public bool IsIntersected(Collision compareObject) =>
+            _collisionBox.IntersectsWith(compareObject._collisionBox);
     }
 }
