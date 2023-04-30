4ec5e5a63f08c99bb16c97db2b68a8ea49797e7e
H1Kt0 <konovalchuk.denis@gmail.com>
2023-04-30 23:35:42 +0300

    Добавлен класс текстуры. Сделан тестовый бэкграунд, также тестовая текстура горы.

--- Game_Engine_Library/Texture.cs
+++ Game_Engine_Library/Texture.cs
@@ -16,16 +16,6 @@ namespace Game_Engine_Library {
         public virtual int ID { get; private set; }
 
         /// <summary>
-        /// Ширина текстуры.
-        /// </summary>
-        public int Width { get; private set; }
-
-        /// <summary>
-        /// Высота текстуры.
-        /// </summary>
-        public int Height { get; private set; }
-
-        /// <summary>
         /// Создание новой текстуры.
         /// </summary>
         /// <param name="id">Идентификатор текстуры.</param>
@@ -33,8 +23,6 @@ namespace Game_Engine_Library {
         /// <param name="height">Высота текстуры.</param>
         public Texture(int id, int width, int height) {
             ID = id;
-            Width = width;
-            Height = height;
         }
 
         /// <summary>
